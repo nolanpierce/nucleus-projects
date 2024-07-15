8fb7f27b24dfceb04cb619b6ef443f5dc5c57bb9
bobby rayyy <nolanpierce2828@gmail.com>
2024-07-14 21:47:51 -0700

    swapped accounts to this account so can seperate it from personal projects

--- nucleus-driver/src/entry.cpp
+++ nucleus-driver/src/entry.cpp
@@ -1,7 +1,7 @@
 #include <ntddk.h>
 #include "data_types.h"
 #include "nucleus/impl/assembly.h"
-
+#include "nucleus/eac/eac.h"
 
 typedef struct {
 	void*	StartAddress;
@@ -14,5 +14,14 @@ typedef struct {
 NTSTATUS driver_entry(custom_entry* info) {
 	if (!info || !info->StartAddress || !info->loader_process_id || !!info->winver) return STATUS_FAILED_DRIVER_ENTRY;
 	
+	
+	nucleus::easyanticheat::initialize();
+
+	//do our comms thread then we restore
+	// 
+	// 
+	// 
+	//restore after
+	nucleus::easyanticheat::cleanup();
 	return STATUS_SUCCESS;
 }
